c0beb3c8162b23eab1587d92cd0f6b5eecb90c3e
polgerg <polonkai.gergely92@gmail.com>
2022-10-20 11:36:20 +0200

    cart-summary

--- src/app/components/pages/cart/cart.component.ts
+++ src/app/components/pages/cart/cart.component.ts
@@ -13,4 +13,26 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getNetSubtotal(): number{
+    let subTotal = 0
+    this.cartService.inCart.forEach(item => subTotal += item.beer.price * item.quantity)
+    return Math.round(subTotal*100)/100;
+  }
+
+  calcVAT(): number {
+    return Math.round((this.getNetSubtotal() * 0.2)*100)/100; 
+  }
+
+  isFreeShipping(): boolean {
+    return (this.getNetSubtotal() + this.calcVAT()) >= 500
+  }
+
+  getOrderTotal(): number {
+    return this.isFreeShipping() ? Math.round((this.getNetSubtotal() + this.calcVAT())*100)/100 : Math.round((this.getNetSubtotal() + this.calcVAT() + 10)*100)/100 
+  }
+
+  checkout() {
+    console.log('We will navigate you to the checkout page soon..')
+  }
+
 }
